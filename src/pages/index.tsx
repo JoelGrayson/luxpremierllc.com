cdd023e538e1492b93eaf5780bd0d3893699053e
Joel Grayson <joel@joelgrayson.com>
2025-03-28 11:31:49 -0400

    added venderoo and edit time

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,18 +10,26 @@ export default function Home() {
             <title>Lux Premier LLC</title>
         </Head>
         <h1 className='text-center mt-10'>Lux Premier LLC</h1>
-        <p className='text-xl leading-relaxed mb-5'>Lux Premier is a company in New York State that started operations in 2019. It is led by Joel Grayson and his father sidekick, Paul Grayson. It is the parent company of three subsidiaries:</p>
-        <section className='flex justify-around'>
+        <p className='text-xl leading-relaxed mb-5'>Lux Premier is a company in New York State that started operations in 2019. Here are its various projects:</p>
+        <section className='grid grid-cols-3 gap-3'>
+            <Subsidiary color={'#ebedf0'} description={'App for changing file date metadata'} href='https://joelgrayson.com/software/edit-time'>
+                <Image src='/images/edit-time.png' alt='Edit Time logo' height={60} width={60} />
+                <h2 className='text-2xl text-black mt-3 relative'>Edit Time</h2>
+            </Subsidiary>
+            <Subsidiary color={'#d8ac7e'} description={'Vending machine that sold snacks in school hallway'} href='https://www.youtube.com/watch?v=bHHk2FL5Ujs'>
+                <Image src='/images/venderoo.png' alt='Venderoo photo' height={60} width={60} />
+                <h2 className='text-2xl text-black mt-3 relative'>Venderoo</h2>
+            </Subsidiary>
             <Subsidiary color={'#b32636'} description={'Online painting gallery selling canvas prints for home decor'} href='https://lirongart.com'>
-                <Image src='/images/lirong-art.gif' alt='lirong-art-logo' height={35} width={28} />
+                <Image src='/images/lirong-art.gif' alt='Lirong Art Logo' height={35} width={28} />
                 <h2 className='text-2xl text-white mt-3 relative'>Lirong Art<Com /></h2>
             </Subsidiary>
             <Subsidiary color={'#afd0f1'} description={'Find which bus to take'} href='https://buseroo.com'>
-                <Image src='/images/buseroo.png' alt='lirong-art-logo' height={60} width={60} />
+                <Image src='/images/buseroo.png' alt='Buseroo logo' height={60} width={60} />
                 <h2 className='text-2xl text-black mt-3 relative'>Buseroo<Com /></h2>
             </Subsidiary>
             <Subsidiary color={'#eee'} description={'Online t-shirt shop'} href='https://shirtocracy.com'>
-                <Image src='/images/shirtocracy.png' alt='lirong-art-logo' height={75} width={75} />
+                <Image src='/images/shirtocracy.png' alt='Shirtocracy logo' height={75} width={75} />
                 <h2 className='text-2xl mt-3 relative'>Shirtocracy<Com /></h2>
             </Subsidiary>
         </section>
